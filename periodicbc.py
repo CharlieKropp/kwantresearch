915285bb605f0fc626fecd76b3fe050a2292c9e1
Charlie <Charles Kropp>
2020-10-26 19:46:49 -0500

    Working PBC

--- periodicbc.py
+++ periodicbc.py
@@ -1,15 +1,12 @@
 import kwant
 from matplotlib import pyplot
 import numpy as np
-a = 1
-t = 1.0
-L = 30
-W = 10
+import scipy.io as sio
 
 def plot_conductance(syst, energies):
     data = []
     for energy in energies:
-        smatrix = kwant.smatrix(syst, energy)
+        smatrix = kwant.smatrix(syst, energy,params=dict(k_x = 0, k_y = 0))
         data.append(smatrix.transmission(1, 0))
 
     pyplot.figure()
@@ -18,27 +15,33 @@ def plot_conductance(syst, energies):
     pyplot.ylabel("conductance [e^2/h]")
     pyplot.show()
 
-lat = kwant.lattice.square(a)
+def make_system(a=1, t=1.0, L=30, W=10):
+    lat = kwant.lattice.square(a)
 
-syst = kwant.Builder(kwant.TranslationalSymmetry(lat.vec((0,L))))
-syst[(lat(x, y) for x in range(L) for y in range(W))] = 4 * t
-syst[lat.neighbors()] = -t
+    syst = kwant.Builder(kwant.TranslationalSymmetry(lat.vec((0,W))))
+    syst[(lat(x, y) for x in range(L) for y in range(W))] = 4 * t
+    syst[lat.neighbors()] = -t
 
-syst = kwant.wraparound.wraparound(syst)
+    syst = kwant.wraparound.wraparound(syst)
 
-leadL = kwant.Builder(kwant.TranslationalSymmetry((-1,0), lat.vec((0,W))))
-leadL[(lat(0, j) for j in range(W))] = 4 * t
-leadL[lat.neighbors()] = -t
+    leadL = kwant.Builder(kwant.TranslationalSymmetry((-1,0), lat.vec((0,W))))
+    leadL[(lat(0, j) for j in range(W))] = 4 * t
+    leadL[lat.neighbors()] = -t
 
-syst.attach_lead(kwant.wraparound.wraparound(leadL, keep=0))
+    syst.attach_lead(kwant.wraparound.wraparound(leadL, keep=0))
 
-leadR = kwant.Builder(kwant.TranslationalSymmetry((1,0), lat.vec((0,W))))
-leadR[(lat(0, j) for j in range(W))] = 4 * t
-leadR[lat.neighbors()] = -t
+    leadR = kwant.Builder(kwant.TranslationalSymmetry((1,0), lat.vec((0,W))))
+    leadR[(lat(0, j) for j in range(W))] = 4 * t
+    leadR[lat.neighbors()] = -t
 
-syst.attach_lead(kwant.wraparound.wraparound(leadR, keep=0))
+    syst.attach_lead(kwant.wraparound.wraparound(leadR, keep=0))
 
+    syst = syst.finalized()
+    return syst
+
+syst = make_system()
 #kwant.plot(syst)
-syst = syst.finalized()
-#wf1 = kwant.solvers.default.wave_function(syst, energy=.2)(0)
-plot_conductance(syst, energies=[.01 * i for i in range(200)])
+wf1 = kwant.solvers.default.wave_function(syst, energy=1, params=dict(k_x = 1, k_y = 1))(0)
+#plot_conductance(syst, energies=[.01 * i for i in range(100)])
+kwant.plotter.map(syst, np.real(wf1[1]), cmap='jet')
+#sio.savemat('wf/'+'wf9.mat', {'wf1':wf1})
--- wf/scripts/modesofwaveguide.m
+++ wf/scripts/modesofwaveguide.m
@@ -19,11 +19,13 @@ subplot(3,1,3);pcolor( abs(psi).^2);axis equal tight;shading flat;colorbar;color
  
 % Periodic boundary conditions (along y axis)
 Np  = 2*floor(W/lambda)+1;   % Number of waveguide modes
-n   = 0;                     % Allowed range -(Np-1)/2 <= n <= (Np-1)/2
-ky  = 2*pi/W*n;
-kx  = sqrt(k0^2-ky^2);
-psi = sqrt(1/W)*exp(1i*ky*y).*exp(1i*kx*x);
+
+n1   = -1;                     % Allowed range -(Np-1)/2 <= n <= (Np-1)/2
+ky1  = 2*pi/W*n1;
+kx1  = sqrt(k0^2-ky1^2);
+psi1 = sqrt(1/W)*exp(1i*ky1*y).*exp(1i*kx1*x);
+
 figure(2);
-subplot(3,1,1);pcolor(real(psi)   );axis equal tight;shading flat;colorbar;colormap('jet');
-subplot(3,1,2);pcolor(imag(psi)   );axis equal tight;shading flat;colorbar;colormap('jet');
-subplot(3,1,3);pcolor( abs(psi).^2);axis equal tight;shading flat;colorbar;colormap('jet');
+subplot(3,1,1);pcolor(real(psi1)   );axis equal tight;shading flat;colorbar;colormap('jet');
+subplot(3,1,2);pcolor(imag(psi1)   );axis equal tight;shading flat;colorbar;colormap('jet');
+subplot(3,1,3);pcolor(real(psi1)   );axis equal tight;shading flat;colorbar;colormap('jet');
